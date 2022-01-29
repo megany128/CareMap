beafbe1ac24e7b28d4d3b3530ec6ceb93beefade
Megan Yap <maqzyap04@aism.edu.my>
2022-01-30 01:50:14 +0800

    sms verification works

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import Map from './src/screens/Map';
 import AddMarker from './src/screens/AddMarker';
+import PhoneNumber from "./src/screens/PhoneNumber";
+import Otp from "./src/screens/Otp";
+import Gated from "./src/screens/Gated";
 
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -12,6 +15,10 @@ export default function App() {
   return(
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="PhoneNumber" component={PhoneNumber} options={{headerShown: false}}/>
+        <Stack.Screen name="Otp" component={Otp} options={{headerShown: false}}/>
+        <Stack.Screen name="Gated" component={Gated} options={{headerShown: false}}/>
+
         <Stack.Screen name="Map" component={Map} />
         <Stack.Screen name="Request Help" component={AddMarker} options={{headerShown: true}}/>
       </Stack.Navigator>
